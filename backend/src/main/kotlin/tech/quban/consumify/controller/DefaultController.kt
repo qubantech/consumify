366776c12a55360b3a71c1a61c8135fb49553f55
Lihogub Oleg <oligli@yandex.ru>
2021-12-10 23:42:38 +0300

    added swagger ui and dummy controller swagger-ui definition

--- backend/src/main/kotlin/tech/quban/consumify/controller/DefaultController.kt
+++ backend/src/main/kotlin/tech/quban/consumify/controller/DefaultController.kt
@@ -1,5 +1,8 @@
 package tech.quban.consumify.controller
 
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.responses.ApiResponse
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -9,5 +12,7 @@ import org.springframework.web.bind.annotation.RestController
 class DefaultController() {
 
     @GetMapping
+    @Operation(summary = "Example summary.", description = "Example description.")
+    @ApiResponse(responseCode = "200", description = "Example response description")
     fun index(): String = "Hello!"
 }
