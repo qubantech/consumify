4f9c664746a074ee7b3f50e0f8292d849849fa81
Lihogub Oleg <oligli@yandex.ru>
2021-12-10 23:47:14 +0300

    added tests

---
+++ backend/src/test/kotlin/tech/quban/consumify/ConsumifyApplicationTests.kt
@@ -0,0 +1,13 @@
+package tech.quban.consumify
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ConsumifyApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
