8fd994920be7b71efbc7bf2ea6894db114dcbfdc
v.shustov <57774085+shuimi@users.noreply.github.com>
2021-12-11 15:25:31 +0300

    -minor fix

--- frontend/src/components/overlays/modal-cash-backs.tsx
+++ frontend/src/components/overlays/modal-cash-backs.tsx
@@ -1,4 +1,5 @@
-import {observer} from "mobx-react-lite";
+import React from 'react';
+
 import {
     Center,
     Divider,
@@ -12,88 +13,92 @@ import {
     Text,
     Title,
     useMantineTheme
-} from "@mantine/core";
-import React from "react";
-import {storeCashBacks} from "../../store/cashbacks-list";
-import mir from "../../meta/mir-logo.svg"
-import {CardsCashbackPartners} from "../common/cards-cashback-partners";
-import {CardsCashbackCategories} from "../common/cards-cashback-categories";
-import {SelectSubscriptions} from "../common/select-subscriptions";
-import {storeProfile} from "../../store/profile";
+} from '@mantine/core';
+
+import { observer } from 'mobx-react-lite';
+import { storeCashBacks } from '../../store/cashbacks-list';
+import { storeProfile } from '../../store/profile';
+
+import mir from '../../assets/mir-logo.svg'
+
+import { CardsCashbackPartners } from '../common/cards-cashback-partners';
+import { CardsCashbackCategories } from '../common/cards-cashback-categories';
+import { SelectSubscriptions } from '../common/select-subscriptions';
+
 
 export const ModalCashBacks = observer(() => {
 
-    const {isOpen, setIsOpen} = storeCashBacks
+    const { isOpen, setIsOpen } = storeCashBacks
     const theme = useMantineTheme();
 
     return (
         <Modal
-            transition="fade"
+            transition='fade'
             transitionDuration={1000}
-            transitionTimingFunction="ease"
+            transitionTimingFunction='ease'
             opened={isOpen}
-            size={"xl"}
+            size={'xl'}
             onClose={() => setIsOpen(false)}
-            title={"Cashbacks"}>
+            title={'Cashbacks'}>
             {storeProfile.id != 0 ?
-            (<>
-                <Center>
-                <Paper color={"green"} padding="xs" shadow="xl" radius="md" withBorder>
-                    <Group>
-                        <Image src={mir} height={20}/>
-                        <Group>
-                            <Text>****4956</Text>
-                            <Text color={"gray"}>
-                                10/23
-                            </Text>
-                            <Menu>
-                                <Menu.Item color={"red"}> Удалить карту </Menu.Item>
-                            </Menu>
+                (<>
+                    <Center>
+                        <Paper color={'green'} padding='xs' shadow='xl' radius='md' withBorder>
+                            <Group>
+                                <Image src={mir} height={20}/>
+                                <Group>
+                                    <Text>****4956</Text>
+                                    <Text color={'gray'}>
+                                        10/23
+                                    </Text>
+                                    <Menu>
+                                        <Menu.Item color={'red'}> Удалить карту </Menu.Item>
+                                    </Menu>
+                                </Group>
+                            </Group>
+                        </Paper>
+                    </Center>
+                    <Space h={'xs'}/>
+                    <Center>
+                        <Group spacing={'xs'} noWrap>
+                            <Title order={5}> Было сэкономлено:</Title>
+                            <Text size={'xl'} weight={'800'} color={'green'}>432 рубля</Text>
                         </Group>
-                    </Group>
-                </Paper>
-            </Center>
-            <Space h={"xs"}/>
-            <Center>
-                <Group spacing={"xs"} noWrap>
-                    <Title order={5}> Было сэкономлено:</Title>
-                    <Text size={"xl"} weight={"800"} color={"green"}>432 рубля</Text>
-                </Group>
-            </Center>
-            <Space h={"xs"}/>
-            <Tabs color={theme.primaryColor} grow>
-                <Tabs.Tab label={"Кэшбек от партнеров"}>
-                    <Divider size={"xl"}
-                             labelPosition={"center"}
-                             label={<Title order={6}>Персональный кэшбек</Title>}/>
-                    <Space h={"md"}/>
-                    <CardsCashbackPartners/>
-                    <Space h={"xs"}/>
-                    <Divider size={"xl"}
-                             labelPosition={"center"}
-                             label={<Title order={6}>Повышенный кэшбек</Title>}/>
-                    <Space h={"md"}/>
-                    <CardsCashbackPartners/>
-                    <CardsCashbackPartners/>
-                    <CardsCashbackPartners/>
-                    <CardsCashbackPartners/>
-                </Tabs.Tab>
-                <Tabs.Tab label={"Кэшбек от банка"}>
-                    <Divider size={"xl"}
-                             labelPosition={"center"}
-                             label={<Title order={6}>Подписка на любимые категории</Title>}/>
-                    <Space h={"md"}/>
-                    <SelectSubscriptions/>
-                    <Space h={"xs"}/>
-                    <Divider size={"xl"}
-                             labelPosition={"center"}
-                             label={<Title order={6}>Действующие подписки</Title>}/>
-                    <Space h={"md"}/>
-                    <CardsCashbackCategories/>
-                </Tabs.Tab>
-            </Tabs>
-            </>)
-            :
+                    </Center>
+                    <Space h={'xs'}/>
+                    <Tabs color={theme.primaryColor} grow>
+                        <Tabs.Tab label={'Кэшбек от партнеров'}>
+                            <Divider size={'xl'}
+                                     labelPosition={'center'}
+                                     label={<Title order={6}>Персональный кэшбек</Title>}/>
+                            <Space h={'md'}/>
+                            <CardsCashbackPartners/>
+                            <Space h={'xs'}/>
+                            <Divider size={'xl'}
+                                     labelPosition={'center'}
+                                     label={<Title order={6}>Повышенный кэшбек</Title>}/>
+                            <Space h={'md'}/>
+                            <CardsCashbackPartners/>
+                            <CardsCashbackPartners/>
+                            <CardsCashbackPartners/>
+                            <CardsCashbackPartners/>
+                        </Tabs.Tab>
+                        <Tabs.Tab label={'Кэшбек от банка'}>
+                            <Divider size={'xl'}
+                                     labelPosition={'center'}
+                                     label={<Title order={6}>Подписка на любимые категории</Title>}/>
+                            <Space h={'md'}/>
+                            <SelectSubscriptions/>
+                            <Space h={'xs'}/>
+                            <Divider size={'xl'}
+                                     labelPosition={'center'}
+                                     label={<Title order={6}>Действующие подписки</Title>}/>
+                            <Space h={'md'}/>
+                            <CardsCashbackCategories/>
+                        </Tabs.Tab>
+                    </Tabs>
+                </>)
+                :
                 (<Text> You not authorized :(</Text>)
             }
         </Modal>
